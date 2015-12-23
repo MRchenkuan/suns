ad3e381642c7749c39eaaf19efbe55b8e6c7f880
chenkuan <chenkuan@gigold.com>
2015-12-23 14:29:35 +0800

    data数据排序

--- introduction.php
+++ introduction.php
@@ -23,6 +23,25 @@
             -khtml-user-select:none;/*早期浏览器*/
             user-select:none;
         }
+        /* 电视局部样式 */
+        .imgOfDetail{width: 100%;}
+        #tvDetail{display: none;position:absolute;top: 80px;left: 15%;width: 70%;z-index: 1000}
+        #tvDetail iframe,#tvDetail .imgwrap{background: #ffffff;float: left;width: 100%;height: 100%;border-radius: 5px;border:1px solid #cccccc;overflow: hidden;}
+        #tvDetail iframe{min-height: 750px;}
+        #tvDetail .imgwrap{min-height: 550px;}
+        #tvDetail .imgwrap img{width: 100%;margin: 0 auto;}
+        #closeShell{display: none;background: #15192F;opacity:.8;position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index: 999;}
+        #tvDetail .siderbar{width: 28%;float: right;min-height: 200px;background: white;border-radius: 5px;border:1px solid #cccccc;overflow: hidden;}
+        #filelist {overflow: hidden;_zoom:1;margin: 20px;padding-bottom: 20px;border-bottom: 1px solid #cccccc;}
+        #filelist li{cursor: pointer;white-space: nowrap;overflow: hidden;text-overflow:ellipsis;;background: #F7F3EF;margin-bottom: 5px;display: block;border-radius: 5px;padding: 5px 5%;width: 90%;}
+        #filelist li:hover{color:#fff;background: #E69220;transition:all .5s ease}
+        .sideBarHtmlBox{margin: 20px;overflow: hidden;_zoom:1;}
+        #sideBarHead {border-bottom: 1px #cccccc solid}
+        #sideBarHead p,#sideBarFoot p{color: grey;font-size: 12px;margin-bottom: 10px;}
+        #sideBarHead div{width: 100%;}
+        #sideBarHead img{height: 50px;float: left}
+        #sideBarHead span{max-width: 69%;white-space: nowrap;overflow: hidden;text-overflow:ellipsis;margin-left: 10px;float: left;font-size: 18px;line-height: 50px;}
+        /* -- */
     </style>
 </head>
 <body style="min-width: 768px">
@@ -36,7 +55,7 @@
 //    $menuList=Array(
 //        '公司简介'=>'part-companyintro.html',
 //        '顺风团队'=>'part-sunsgroup.html',
-//        '顺风活动'=>'part-evt.html',
+//        '顺风活动'=>'part-evt.php',
 //        '公司荣誉'=>'part-honer.html',
 //        '公司环境'=>'part-env.html',
 //        '集团化运营'=>'part-jthyy.html'
@@ -49,7 +68,7 @@
         <ul class="content-nav cannot_select">
             <li id="currentA" data-url='./partpage/part-companyintro.html'>公司简介</li>
             <li data-url='./partpage/part-sunsgroup.html'>顺风团队</li>
-            <li data-url='./partpage/part-evt.html'>顺风活动</li>
+            <li data-url='./partpage/part-evt.php'>顺风活动</li>
             <li data-url='./partpage/part-honer.html'>公司荣誉</li>
             <li data-url='./partpage/part-env.html'>公司环境</li>
             <li data-url='./partpage/part-jthyy.html'>集团化运营</li>
@@ -73,4 +92,6 @@
 
 
 <?php include './widget/linksModel.php';?>
+<div id="closeShell"></div>
+<div id="tvDetail" class="clearfix"></div>
 <?php include './widget/foot.php';?>
---
+++ partpage/part-evt.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link href="../css/partpage.css" type="text/css" rel="stylesheet" >
+    <script src="../js/jquery.js"></script>
+    <title>顺风传媒 - 首页</title>
+    <style>
+        body,p{padding: 0;margin: 0}
+        ul{padding: 0;margin: 0}
+        li{list-style: none;float: left}
+        a{text-decoration: none;color: inherit;display: block}
+        .block{position: relative;}
+        .clearfix{overflow:hidden;_zoom:1;}
+        .detail div{height: 240px;float: left;line-height: 1.5;}
+        .detail div img{width: 100%;height: 100%;border: 0}
+        .eventCover:hover p{height:48px;transition: all .4s ease}
+        .eventCover p{height:32px;transition: all .4s ease}
+    </style>
+</head>
+<body>
+<?php
+# 设置路径
+function getPureDirs($path){
+    $_eventFiles = array();
+    $eventFiles = scandir($path);
+    foreach($eventFiles as $k=>$v) {
+        if ($v != '.' && $v != '..' && is_dir($path.$v)) {
+            array_push($_eventFiles, $v);
+        }
+    }
+    return $_eventFiles;
+}
+
+function getPureFiles($path){
+    $_eventFiles = array();
+    $eventFiles = scandir($path);
+    foreach($eventFiles as $k=>$v) {
+        if ($v != '.' && $v != '..' && $v!=".DS_Store") {
+            array_push($_eventFiles, $v);
+        }
+    }
+    return $_eventFiles;
+}
+
+$eventRoot = '../data/event/';
+$eventFiles = getPureDirs($eventRoot);
+# json
+$eventFilesForJson = array();
+# 初始化文件夹列表
+foreach($eventFiles as $k=>$v) {
+    if (is_dir($eventRoot.$v)) {
+        scandir($eventRoot.$v);
+        ?>
+        <div class="eventCover detail-3-col" data-tar="<?php echo $v?>"><img src="../data/event/<?php echo $v?>/封面.jpg"><p><?php echo $v?></p></div>
+        <?php
+    }
+    # 组成json
+    $JPGS = getPureFiles($eventRoot.$v);
+    $eventFilesForJson[$v] = $JPGS;
+}
+?>
+<script>
+
+    (function(){
+        var eventData = eval(<?php echo json_encode($eventFilesForJson);?>);
+        console.log(eventData);
+
+        /* 初始化信息栏 */
+        var tvDetail = window.parent.document.querySelector('#tvDetail');
+        var closeShell = window.parent.document.querySelector('#closeShell');
+        var parentWin = window.parent;
+        if(!tvDetail)throw '找不到父框架指定容器#tvDetail';
+        if(!closeShell)throw '找不到父框架指定容器#closeShell';
+        if(!parentWin)throw '找不到父框架';
+
+        Array.prototype.some.call(document.querySelectorAll('.eventCover'),function(it){
+            it.addEventListener('click',showDetail)
+        });
+
+        /*工具方法 - 展示子频道细节*/
+        function showDetail() {
+            /* 重置细节内容页 */
+            tvDetail.innerHTML = '';
+            tvDetail.style.display = 'block';
+            var eventName = this.getAttribute('data-tar');
+            var file = eventData[eventName];
+            /*初始化遮罩*/
+            parentWin.scrollToTop();
+            $(closeShell).fadeIn();
+            closeShell.addEventListener('click', function () {
+                $(closeShell).fadeOut();
+                $(tvDetail).fadeOut();
+            });
+            /* ---- */
+
+            var eventContent = document.createElement('div');
+            for(var i=0;i<file.length;i++){
+                var img = document.createElement('img');
+                img.src ='./data/event/'+eventName+"/"+file[i];
+                img.setAttribute('class','imgOfDetail');
+                eventContent.appendChild(img);
+            }
+            tvDetail.appendChild(eventContent);
+        }
+    })()
+</script>
+</body>
+</html>
